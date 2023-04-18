7e4871d6004b471aa2c03240b5ab4dfae8c41922
Christian M Rapp <106698098+CMRapp@users.noreply.github.com>
2023-04-18 15:16:53 -0400

    commit of completed wk 4 coding

--- week04Coding.js
+++ week04Coding.js
@@ -176,8 +176,13 @@ console.log(divider + '\n** Question 13 **\n' + divider);
 console.log('Create a function of your own that solves a problem.   In comments, write what the function does and why you created it.');
 
 /** CUSTOM METHOD #1 | showMainMenu
- *  Displays a menu for the program
+ *  An exercise in using Template Literals. Displays a menu for the program
+ * I originally was going to use this menu to allow user input to select the assignment
+ * they wanted to view. However to do that properly, the use of HTML/CSS would be needed
+ * to style the message box to allow the entire menu to be displayed.
+ * Therefore, this menu (although not being used) will display in the console window.
 */  
+console.log('CUSTOM METHOD #1: showMainMenu Function | displays a menu using a Template Literal. \n');
 
 //create a template literal to display main menu
 const showMainMenu = () => {
@@ -194,8 +199,79 @@ const showMainMenu = () => {
     11) Question 11 | Average of Two Arrays Function
     12) Question 12 | willBuyDrink Function`;
 
-    window.prompt(menu);      //display menu on console
-    
-
+    // window.prompt(menu);      //display menu in messagebox
+    console.log(menu);
 }
 showMainMenu();      //call showMenu function
+
+/** CUSTOM METHOD #2 createNonet | Uses rendom numbers to create a 3x3 soduku nonet.
+ *  Numbers must be from 1 to 9, with no duplicates.  Function uses arrays and math.random function. 
+ *  Function will call the boolean function numExists to determine in the generated random number has already been chosen
+ *  (accepts nonet array & chosen number as a parameter).
+ * 
+ *  Function wil call the displayNonet function to display the created nonet. (accepts created nonet array as param)
+ * 
+ *  This is an adaptation form an something I created tn the BE course learning Java using multidimensonal arrays.
+ *  I've discovered that md arrays do not exist in JS natively. I adapted my solution using a standard array and a template
+ *  literal for displaying the nonet. This is actually less lines of code than what was in my Java solution.
+ *  
+ *  I created this function because I've always wanted to create a Soduku program. Creating this function is the first 
+ *  step in creating the program. The second step would be to create the 9x9 grid with 9 nonets. The numbers in all
+ *  nonets would need to be checked for duplicates in each row & column.
+ * 
+ *  NOTE: JavaScript Math.random() always returns a number lower than 1. To return random integers, 
+ *  Math.random() must be used with Math.floor()
+ * 
+ *  NOTE: The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+ *  */
+
+console.log('CUSTOM METHODS #2,3: createNonet, displayNonet \nCreates and displays a 3x3 standard Soduku nonet\n');
+
+// displayNonet accepts the passed in nonet array and displays it in a 3x3 grid
+function displayNonet(nonetArr) {
+
+    console.log('GENERATED SUDUKU NONET:\n');
+    
+    let displayStr1 = ` `;                        //create a template literals to hold display string
+    let displayStr2 = ``;
+    let displayStr3 = ``;
+
+    //display nonet to console
+    for (let i=0; i < nonetArr.length; i++) {
+        let num = nonetArr[i];                  //create variable for template literal
+        
+        if (i < 3) {
+            displayStr1 += `${num} `;           //create template literal using current value of array index  
+            
+        } else if (i < 6 ){
+            displayStr2 += `${num} `;  
+        } else {
+            displayStr3 += `${num} `; 
+        }
+            
+    }
+    console.log(displayStr1 + "\n", displayStr2 +"\n", displayStr3+"\n");
+}
+
+// createNonet picks random numbers 1-9 and builds a 3x3 Soduku nonet without duplicates
+const createNonet = () => {
+
+    let nonet = [];                                      //create an array to hold 9 elements
+    let counter = 1;                                     //counter to increment loop after valid number is stored
+
+    do {                                                //begin for loop to pick and assign numbers for the array
+        let num = (Math.floor(Math.random() * 9)+1);    //generate random number from 1-9
+        if (!nonet.includes(num)) {
+            nonet.push(num);
+            counter ++;
+        }
+    } while (counter < 10);
+    console.log('CREATED ARRAY: ' + nonet);
+    displayNonet(nonet);
+}
+
+createNonet();  
+let links=`RESEARCH LINKS:
+https://www.w3schools.com/js/js_random.asp
+https://www.geeksforgeeks.org/how-to-create-an-array-containing-non-repeating-elements-in-javascript/`;
+console.log(links);                                       ;
